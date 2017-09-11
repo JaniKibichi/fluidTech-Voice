ba778337f3a2357dab08439a2063160323daf7b4
janikibichi <janikibichi@gmail.com>
2017-09-11 13:58:56 +0300

    voice ivr

---
+++ ivr.php
@@ -0,0 +1,84 @@
+<?php
+
+//AT POST
+$recordingUrl = $_POST['sessionId'];
+$isActive  = $_POST['isActive'];
+$direction = $_POST['direction'];
+$callerNumber = $_POST['callerNumber'];
+$destinationNumber = $_POST['destinationNumber'];
+$dtmfDigits  = $_POST['dtmfDigits'];
+$recordingUrl = $_POST['recordingUrl'];
+$durationInSeconds  = $_POST['durationInSeconds'];
+$currencyCode  = $_POST['currencyCode'];
+$amount  = $_POST['amount'];
+
+//PARAMS
+require_once('AfricasTalkingGateway.php');
+$username   = "WeloveNerds";
+$apikey     = "34916511ccfe864b9aed8cb41758c5c87638daf3867148dea6c951a38e2ff126";
+$from = "+254711082303";
+//call to db
+$to   = "+254708415904,+254732730473";
+// Create a new instance of our awesome gateway class
+$gateway = new AfricasTalkingGateway($username, $apikey);
+
+
+switch($dtmfDigits){
+    case "0":
+    // Talk to sales... Compose the response
+    $response  = '<?xml version="1.0" encoding="UTF-8"?>';
+    $response .= '<Response>';
+    $response .= '<Say>Please hold while we connect you to Sales.</Say>';
+    $response .= '<Dial sequential="true" phoneNumbers="880.welovenerds@ke.sip.africastalking.com,+254787235065" ringbackTone="http://localhost:8010/media/sautisol.mp3"/>';
+    $response .= '</Dial>';
+    $response .= '</Response>';
+
+    // Print the response onto the page so that our gateway can read it
+    header('Content-type: text/plain');
+    echo $response;
+    break;
+
+    case "1":
+        // Talk to support... Compose the response
+        $response  = '<?xml version="1.0" encoding="UTF-8"?>';
+        $response .= '<Response>';
+        $response .= '<Say>Please hold while we connect you to Support.</Say>';
+        $response .= '<Dial sequential="true" phoneNumbers="880.welovenerds@ke.sip.africastalking.com,+254787235065" ringbackTone="http://localhost:8010/media/sautisol.mp3"/>';
+        $response .= '</Dial>';
+        $response .= '</Response>';
+    
+        // Print the response onto the page so that our gateway can read it
+        header('Content-type: text/plain');
+        echo $response;
+    break;
+
+    case "2":
+        // Talk listen again... Compose the response
+        $response  = '<?xml version="1.0" encoding="UTF-8"?>';
+        $response .= '<Response>';
+        $response .= '<Redirect>http://localhost:8010/pick.php</Redirect>';
+        $response .= '</Response>';
+
+        // Print the response onto the page so that our gateway can read it
+        header('Content-type: text/plain');
+        echo $response;
+        break;
+
+    default:
+        // Talk to support... Compose the response
+        $response  = '<?xml version="1.0" encoding="UTF-8"?>';
+        $response .= '<Response>';
+        $response .= '<Say>Please hold while we connect you to Support.</Say>';
+        $response .= '<Dial sequential="true" phoneNumbers="880.welovenerds@ke.sip.africastalking.com,+254787235065" ringbackTone="http://localhost:8010/media/sautisol.mp3"/>';
+        $response .= '</Dial>';
+        $response .= '</Response>';
+    
+        // Print the response onto the page so that our gateway can read it
+        header('Content-type: text/plain');
+        echo $response;
+    break;
+
+}
+
+
+?>
